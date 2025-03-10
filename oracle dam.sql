49d5d98e635406e2b90b5b9f7ffbe38e93c95fd3
SPFe5067 <lkjpoi3817@gmail.com>
2025-03-10 18:22:52 +0900

    0310

---
+++ oracle dam.sql
@@ -0,0 +1,27 @@
+-- 한줄 주석
+
+/*
+ * 범위 주석
+ */
+
+
+-- 선택한 SQL 수행 : 구문에 커서 두고 CTLR + ENTER
+-- 전체의 SQL 수행 : alt + x
+
+--12c 버전 이전 문법 허용 구문
+
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+--계정 생성 구문(username : kh / password : kh4567)
+CREATE USER workbook IDENTIFIED BY workbook ;
+
+
+GRANT RESOURCE, CONNECT TO workbook;
+--사용자 계정 권한 부여 설정
+--RESOURCE : 테이블이나 인덱스 같은 DB 객체를 생성할 권한
+--CONNECT : DB에 연결하고 로그인 할 수 있는 권한
+
+ALTER USER workbook DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM ;
+--객체가 생성될 수 있는 공간 할당량 무제한 지정
+
+
